99fe1d471dd993e90e4d8f3b5b7692c53a23fa8b
Toever1503 <Haunv1503@gmail.com>
2022-09-23 15:16:29 +0700

    update

---
+++ src/main/java/com/DockerVolumnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerVolumnSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerVolumnSpringbootApplication.class, args);
+    }
+
+}
